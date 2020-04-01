bde972734405caf195baffa1d536629a6817cc39
beksbhale20 <61486131+beksbhale20@users.noreply.github.com>
2020-04-01 22:20:55 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+// ENTER DATA COOKIE
+
+$__cfduid = "__cfduid=d5c2da1413873aa8fe9d58648e38413a71585753794";
+
+$__tawkuuid = "__tawkuuid=e::sonex.cloud::Q0/wJQWnisKJfFqZfpnotrJeJo3wDJXFJNmRX8RPn+PrbXCafUl2d2Av7R4Yy/3M::2";
+
+$_gid = "_gid=GA1.2.793823428.1585753802";
+
+// ENTER DATA LOGIN
+
+$email = "yogiriki88@gmail.com";
+
+$password = "yogi8899";
